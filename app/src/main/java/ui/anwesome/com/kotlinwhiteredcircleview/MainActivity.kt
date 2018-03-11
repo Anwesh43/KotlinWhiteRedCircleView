ab3c4217874075dd9faa0bce3427cc61d114638b
Anwesh43 <anweshthecool0@gmail.com>
2018-03-11 12:24:03 +0530

    adding WhiteRedCircleView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinwhiteredcircleview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinwhiteredcircleview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinwhiteredcircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.whiteredcircleview.WhiteRedCircleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        WhiteRedCircleView.create(this)
     }
 }
