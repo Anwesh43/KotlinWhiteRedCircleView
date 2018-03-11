956bed474cb127acc2a279846b615e7e9dc417f9
Anwesh43 <anweshthecool0@gmail.com>
2018-03-11 10:41:09 +0530

    adding WhiteRedCircleView class

--- whiteredcircleview/src/main/java/ui/anwesome/com/whiteredcircleview/WhiteRedCircleView.kt
+++ whiteredcircleview/src/main/java/ui/anwesome/com/whiteredcircleview/WhiteRedCircleView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.whiteredcircleview
 /**
  * Created by anweshmishra on 11/03/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class WhiteRedCircleView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
